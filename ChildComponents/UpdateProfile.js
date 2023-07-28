e359f9cdf84a7d12d8557959b227f0af5264da63
nedim begic <nedim.begic93@gmail.com>
2023-07-28 23:02:16 +0200

    sendin data to backend and updating the profile

--- ChildComponents/UpdateProfile.js
+++ ChildComponents/UpdateProfile.js
@@ -2,8 +2,38 @@ import styleUpdate from "./UpdateProfile.module.css";
 import { Backdrop } from "./ErrorHandler";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { useRef } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
 
 const UpdateProfile = (props) => {
+  const descRef = useRef();
+  const nameRef = useRef();
+  const router = useRouter();
+
+  const changeInfoHandler = async (e) => {
+    e.preventDefault();
+    const data = {
+      description: descRef.current.value,
+      name: nameRef.current.value,
+    };
+
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_SITE + `/user/${props.profile.nickName}`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${Cookies.get("token")}`,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    props.edit();
+    router.push(`/${props.profile.nickName}`);
+  };
+
   return (
     <div>
       <Backdrop onHide={props.edit} />
@@ -32,9 +62,10 @@ const UpdateProfile = (props) => {
           </div>
         </div>
         <div className={styleUpdate.info}>
-          <form>
+          <form onSubmit={changeInfoHandler}>
             <h5>Bio</h5>
             <textarea
+              ref={descRef}
               defaultValue={props.profile.description}
               name="description"
               placeholder="Change description"
@@ -42,10 +73,12 @@ const UpdateProfile = (props) => {
             <h5>Name</h5>
 
             <input
+              ref={nameRef}
               defaultValue={props.profile.name}
               name="name"
               placeholder="Change name"
             />
+            <button type="submit">Change info</button>
           </form>
         </div>
       </div>
