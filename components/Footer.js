4fa0352401924af45d4a41460bf3e122f820e586
nedim begic <nedim.begic93@gmail.com>
2023-08-01 20:18:08 +0200

    adding error handeling on footer

--- components/Footer.js
+++ components/Footer.js
@@ -8,12 +8,15 @@ import {
 import Cookies from "js-cookie";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import ErrorHandler from "@/ChildComponents/ErrorHandler";
 
 const Footer = () => {
   const router = useRouter();
   const photo = Cookies.get("photo");
   const logedUser = Cookies.get("nickName");
   const [image, setImage] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [errMessage, setErrMessage] = useState("");
   useEffect(() => {
     setImage(photo);
   }, [image, setImage]);
@@ -25,16 +28,38 @@ const Footer = () => {
   const goFeed = () => {
     router.push("/");
   };
+
+  const onSearch = () => {
+    setErrMessage("No search yet.");
+    setShowError(true);
+  };
+
+  const onReel = () => {
+    setErrMessage("No reels yet.");
+    setShowError(true);
+  };
+
+  const onMessage = () => {
+    setErrMessage("No messanger yet.");
+    setShowError(true);
+  };
+
+  const hideError = () => {
+    setShowError(false);
+  };
   return (
     <div className={styleFooter.div}>
       <FontAwesomeIcon onClick={goFeed} icon={faHouse} />
-      <FontAwesomeIcon icon={faMagnifyingGlass} />
-      <FontAwesomeIcon icon={faCirclePlay} />
-      <FontAwesomeIcon icon={faPaperPlane} />
+      <FontAwesomeIcon onClick={onSearch} icon={faMagnifyingGlass} />
+      <FontAwesomeIcon onClick={onReel} icon={faCirclePlay} />
+      <FontAwesomeIcon onClick={onMessage} icon={faPaperPlane} />
       <img
         onClick={toProfile}
         src={process.env.NEXT_PUBLIC_SITE + "/uploads/" + image}
       />
+      <div className={styleFooter.noDiv}>
+        {showError && <ErrorHandler onHide={hideError} message={errMessage} />}
+      </div>
     </div>
   );
 };
