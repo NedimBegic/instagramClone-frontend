104cf8fad2c6af1619ea374d8538e856fe7144aa
nedim begic <nedim.begic93@gmail.com>
2023-08-26 08:44:23 +0200

    fetching paths

--- pages/[nickName]/index.js
+++ pages/[nickName]/index.js
@@ -11,20 +11,9 @@ const User = (props) => {
 };
 
 export async function getStaticPaths() {
-  let data;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE}/user`);
-
-  data = await res.json();
-
   return {
-    // property where we configure if every obj has a path, seting it to false we configure that every path has his value
-    // it is pre-rendering some most visited contents and other is rendering on user request from DB dynamicaly
     fallback: true,
-    // its a must have array in which we have obj
-    paths: data.data.map((user) => ({
-      // we insert a must have params and obj of meetup id
-      params: { nickName: user.nickName },
-    })),
+    paths: [], // No initial paths needed
   };
 }
 
