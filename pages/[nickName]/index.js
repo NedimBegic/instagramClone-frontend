335477d141525d5b353d4805b2e38b7ab470e95b
nedim begic <nedim.begic93@gmail.com>
2024-04-03 00:13:25 +0200

    fixing nickname dinamic page

--- pages/[nickName]/index.js
+++ pages/[nickName]/index.js
@@ -1,55 +1,48 @@
 import Profile from "@/ChildComponents/Profile";
 import Footer from "@/components/Footer";
 import style from "./index.module.css";
-const User = (props) => {
-  if (!props.user) {
-    // Handle the case where data is not available yet
-    return <div className={style.loading}>Loading...</div>;
-  }
-  console.log(props.user.data);
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
+const User = () => {
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const { nickName } = router.query; // Extract nickName from URL query parameters
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_SITE}/user`);
+        const data = await res.json();
+        setUserData(data.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Function to find user by nickName in userData array
+  const findUserByNickName = (nickName) => {
+    return userData.find((user) => user.nickName === nickName);
+  };
+
   return (
     <div className={style.allDiv}>
-      <Profile profile={props.user.data} isYours={props.user.hisProfile} />
-      <Footer />
+      {loading ? (
+        <div className={style.loading}>Loading...</div>
+      ) : (
+        <>
+          {/* Pass the user object corresponding to the nickName in the URL */}
+          <Profile profile={findUserByNickName(nickName)} />
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
-
-export async function getStaticPaths() {
-  let data;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE}/user`);
-
-  data = await res.json();
-
-  return {
-    fallback: true,
-    paths: data.data.map((user) => ({
-      params: { nickName: user.nickName },
-    })),
-  };
-}
-
-export async function getStaticProps(context) {
-  const nickName = context.params.nickName;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE}/user/${nickName}`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZTNiOGM5MWUzYzIyNGU2NDI1YTM3NCIsImlhdCI6MTY5MjY0NzAyNiwiZXhwIjoxNjk1MjM5MDI2fQ.I78P0LaFxbLqdlBUTZw8kqS0EjNQf-xAliLmTYjgg3k`,
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  });
-
-  const data = await res.json();
-  return {
-    props: {
-      // we send this as props to this component
-      user: {
-        ...data,
-      },
-    },
-    revalidate: 2,
-  };
-}
 
 export default User;
