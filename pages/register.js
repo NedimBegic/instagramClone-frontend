db797accd7785ffcac18c8c1d5967871715ce7b7
Nedim Begić <102743689+NedimBegic@users.noreply.github.com>
2024-04-07 18:21:42 +0200

    Update register.js

--- pages/register.js
+++ pages/register.js
@@ -1,11 +1,11 @@
 import styleRegister from "../styles/Register.module.css";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Cookies from "js-cookie";
-import Context from "@/store/createContext";
 
 const Register = () => {
   const [errMessage, setErrMessage] = useState("");
+  const [loading, setLoading] = useState(false); // State for loading indicator
   const nickNameRef = useRef();
   const nameRef = useRef();
   const emailRef = useRef();
@@ -13,7 +13,24 @@ const Register = () => {
   const passRef2 = useRef();
   const router = useRouter();
 
-  // go to login route
+  useEffect(() => {
+    // Wake up backend server when the component is mounted
+    const wakeUpServer = async () => {
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_SITE}/user`);
+        if (!res.ok) {
+          throw new Error("Failed to wake up server");
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        console.log("Server started");
+      }
+    };
+
+    wakeUpServer();
+  }, []);
+
   const toLoginHandler = () => {
     router.push("login");
   };
@@ -21,46 +38,57 @@ const Register = () => {
   const submitRegister = async (e) => {
     e.preventDefault();
     if (passRef.current.value !== passRef2.current.value) {
-      setErrMessage("Passwords not match");
+      setErrMessage("Passwords do not match");
       return;
     }
+    setLoading(true);
+
     const data = {
       nickName: nickNameRef.current.value,
       name: nameRef.current.value,
       email: emailRef.current.value,
       password: passRef.current.value,
     };
-    // send data
-    const response = await fetch(
-      process.env.NEXT_PUBLIC_SITE + "/auth/register",
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    );
-    // Response data
-    const responseData = await response.json();
-    /* Check if there is an error */
-    if (responseData.message) {
-      console.log(responseData.message);
-      setErrMessage(responseData.message);
-      return;
-    }
 
-    Cookies.set("token", `${responseData.token}`, { expires: 7 });
-    Cookies.set("photo", `${responseData.user.photo}`, { expires: 7 });
-    Cookies.set("nickName", `${responseData.user.nickName}`, { expires: 7 });
-    router.push("/");
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SITE}/auth/register`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const responseData = await response.json();
+
+      if (responseData.message) {
+        setErrMessage(responseData.message);
+        setLoading(false);
+        return;
+      }
+
+      Cookies.set("token", responseData.token, { expires: 7 });
+      Cookies.set("photo", responseData.user.photo, { expires: 7 });
+      Cookies.set("nickName", responseData.user.nickName, { expires: 7 });
+      router.push("/");
+    } catch (error) {
+      setErrMessage("Error creating account");
+      setLoading(false);
+    }
   };
 
   return (
     <div className={styleRegister.all}>
       <div className={styleRegister.loginPic}>
-        <img src="/login.png" />
+        <img src="/login.png" alt="Login" />
       </div>
       <div className={styleRegister.div}>
         <form onSubmit={submitRegister}>
@@ -69,29 +97,33 @@ const Register = () => {
             ref={nickNameRef}
             name="nickName"
             type="text"
-            placeholder="nickname"
+            placeholder="Nickname"
           />
-          <input ref={nameRef} name="name" type="text" placeholder="name" />
-          <input ref={emailRef} name="email" type="email" placeholder="email" />
+          <input ref={nameRef} name="name" type="text" placeholder="Name" />
+          <input ref={emailRef} name="email" type="email" placeholder="Email" />
           <input
             ref={passRef}
             name="password"
             type="password"
-            placeholder="password"
+            placeholder="Password"
           />
           <input
             ref={passRef2}
             name="password"
             type="password"
-            placeholder="password"
+            placeholder="Confirm Password"
           />
-          <p>{errMessage}</p>
-          <button className={styleRegister.logIn} type="submit">
+          <p>{loading ? "Creating account..." : errMessage}</p>
+          <button
+            className={styleRegister.logIn}
+            type="submit"
+            disabled={loading}
+          >
             Create account
           </button>
         </form>
         <div>
-          <p>Already have an accaunt?</p>
+          <p>Already have an account?</p>
           <button className={styleRegister.logIn} onClick={toLoginHandler}>
             Log in
           </button>
